04614b8ef8d8c144909350c908d30fba6c055b73
JakubC-projects <jakub12134@gmail.com>
2021-05-28 11:28:21 +0200

    localization of home page

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -3,23 +3,23 @@
         <div class="h-4"></div>
         <div class="flex justify-center items-center w-full background-home h-60 sm:h-128">
             <div class="max-w-md text-center text-blue-900 pt-12 sm:pt-32">
-                <h1 class="font-bold pb-5">BCC Voting</h1>
+                <h1 class="font-bold pb-5">{{$t('titles.home')}}</h1>
                 <p class="hidden sm:block">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                    {{$t('descriptions.home')}}
                 </p>
                 <router-link to="/create">
                     <GradButton class="text-2xl sm:text-base sm:mt-4">
-                        Create a voting event
+                        {{$t('actions.create-meeting')}}
                     </GradButton>
                 </router-link>
             </div>
         </div>
         <div class="flex py-4 gap-6 text-gray-700  font-bold justify-center cursor-pointer">
-            <h3 :class="{'text-blue-900': currentTab==='events'}" @click="currentTab='events'">Voting events</h3>
-            <h3 :class="{'text-blue-900': currentTab==='history'}" @click="currentTab='history'">History</h3>
+            <h3 :class="{'text-blue-900': currentTab==='events'}" @click="currentTab='events'">{{$t('actions.show-events')}}</h3>
+            <h3 :class="{'text-blue-900': currentTab==='history'}" @click="currentTab='history'">{{$t('actions.show-history')}}</h3>
         </div>
         <Info>
-            There are no meetings currently available
+            {{$t('info.no-meetings')}}
         </Info>
   </div>
 </template>
