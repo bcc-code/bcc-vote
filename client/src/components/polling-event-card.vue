d974352e8332b115e00c888341e4112bcabcd3e4
Steven Malaihollo <steven.malaihollo@bcc.no>
2021-06-01 09:26:42 +0200

    Added component

---
+++ client/src/components/polling-event-card.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="p-4 border-blue rounded-lg">
+
+    <ArrowRight />
+  </div>
+</template>
+
+<script>
+import ArrowRight from 'heroicons-vue3/solid/ArrowNarrowRightIcon'
+import { defineComponent } from 'vue'
+export default defineComponent({
+    components: {
+        ArrowRight,
+    },
+    data() {
+        return {
+            show: true
+        }
+    }
+})
+</script>
+<style scoped>
+.border-blue {
+  border: 1px solid rgba(22, 23, 26, 0.1);
+}
+</style>
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,34 +1,37 @@
 <template>
   <div>
-        <div class="flex justify-center items-center w-full background-home h-64 sm:h-128">
-            <div class="max-w-md text-center text-blue-900 pt-12 sm:pt-32">
-                <h1 class="font-bold pb-5">{{$t('titles.home')}}</h1>
-                <p class="hidden sm:block">
-                    {{$t('descriptions.home')}}
-                </p>
-                <router-link v-if="$user.authorityLevel <= 5" to="/create">
-                    <button class="gradient-button lg-button text-2xl sm:text-base sm:mt-4">
-                        {{$t('actions.create-meeting')}}
-                    </button>
-                </router-link>
-            </div>
+    <div class="flex justify-center items-center w-full background-home h-64 sm:h-128">
+        <div class="max-w-md text-center text-blue-900 pt-12 sm:pt-32">
+            <h1 class="font-bold pb-5">{{$t('titles.home')}}</h1>
+            <p class="hidden sm:block">
+                {{$t('descriptions.home')}}
+            </p>
+            <router-link v-if="$user.authorityLevel <= 5" to="/create">
+                <button class="gradient-button lg-button text-2xl sm:text-base sm:mt-4">
+                    {{$t('actions.create-meeting')}}
+                </button>
+            </router-link>
         </div>
-        <div class="flex py-4 gap-6 text-gray-700  font-bold justify-center cursor-pointer">
-            <h3 :class="{'text-blue-900': currentTab==='events'}" @click="currentTab='events'">{{$t('labels.voting-events')}}</h3>
-            <h3 :class="{'text-blue-900': currentTab==='history'}" @click="currentTab='history'">{{$t('labels.history')}}</h3>
-        </div>
-        <InfoBox class="m-4">
-            {{$t('info.no-meetings')}}
-        </InfoBox>
+    </div>
+    <div class="flex py-4 gap-6 text-gray-700  font-bold justify-center cursor-pointer">
+        <h3 :class="{'text-blue-900': currentTab==='events'}" @click="currentTab='events'">{{$t('labels.voting-events')}}</h3>
+        <h3 :class="{'text-blue-900': currentTab==='history'}" @click="currentTab='history'">{{$t('labels.history')}}</h3>
+    </div>
+    <InfoBox class="m-4">
+        {{$t('info.no-meetings')}}
+    </InfoBox>
+    <PollingEventCard />
   </div>
 </template>
 
 <script>
 import InfoBox from '../components/info-box'
+import PollingEventCard from '../components/polling-event-card'
 
 export default {
     components: {
         InfoBox,
+        PollingEventCard
     },
     props: {
         mobile: Boolean,
