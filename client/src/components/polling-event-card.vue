5823d94cd06ddb54e6320d7edfab376b22a3f21f
Steven Malaihollo <steven.malaihollo@bcc.no>
2021-06-02 11:47:09 +0200

    Polling card WIP

--- client/src/components/polling-event-card.vue
+++ client/src/components/polling-event-card.vue
@@ -1,37 +1,35 @@
 <template>
-  <div class="px-4 py-6 border-2 border-gray-200 rounded-lg">
-      <h3 class="font-bold">{{pollingEvent.title}}</h3>
-      <div class="text-gray-700 mb-3 flex">
-          <p v-if="pollingEvent.type">{{$t(`labels.${pollingEvent.type}-voting-event`)}}</p>
-          <p v-if="pollingEvent.startDateTime">
-              &nbsp -&nbsp
-              {{new Date(pollingEvent.startDateTime).toLocaleDateString()}}
-            </p>
+    <div class="px-4 py-6 border-2 border-gray-200 rounded-lg">
+        <div class="mb-3 flex justify-between">
+            <h3 class="font-bold">{{pollingEvent.title}}</h3>
+            <div class="px-2 border-2 rounded-lg">
+                <h4 class="font-bold">{{$t(`labels.polling-event-status.${pollingEvent.status}`)}}</h4>
+            </div>
+        </div>
+        <div class="flex justify-center">
+            <button class="gradient-button md-button" @click="goToLobby(pollingEvent._key)">
+                {{$t('actions.participate-in-voting')}}
+            </button>
+        </div>
     </div>
-    <button class="w-32 h-5 font-bold text-blue-900 flex items-center justify-between" @click="goToPoll(pollingEvent._key)">
-        {{$t('actions.go-to-poll')}}
-        <ArrowRight class="h-full"/>
-    </button>
-    <button class="w-32 h-5 font-bold text-blue-900 flex items-center justify-between" @click="editPoll(pollingEvent._key)">
-        Edit poll
-        <ArrowRight class="h-full"/>
-    </button>
-  </div>
 </template>
 
 <script lang="ts">
-import ArrowRight from 'heroicons-vue3/solid/ArrowNarrowRightIcon'
 import { PollingEvent } from '../domain'
 import { defineComponent, PropType } from 'vue'
 export default defineComponent({
-    components: {
-        ArrowRight,
-    },
     props: {
         pollingEvent: Object as PropType<PollingEvent>,
     },
+    data () {
+        return {
+            pollingStatus: {
+                
+            }
+        }
+    },
     methods: {
-        goToPoll(pollingEventKey:string) {
+        goToLobby(pollingEventKey:string) {
             this.$router.push({ path: `/polling-event/lobby/${pollingEventKey}`, params: { id: pollingEventKey}})
         },
         editPoll(pollingEventKey:string) {
