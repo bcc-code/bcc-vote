0a6ef3c857e0b0d2b6aadb38c52ce741c60188f4
Steven Malaihollo <steven.malaihollo@bcc.no>
2021-06-02 17:54:19 +0200

    Vote styling

--- client/src/views/ParticipantLobby.vue
+++ client/src/views/ParticipantLobby.vue
@@ -1,20 +1,21 @@
 <template>
     <section>
-        <div class="max-w-5xl mx-auto py-10 px-4">
-            
-            <div class="text-white mb-12 px-4">
-                <h3 class="font-bold">{{pollingEvent.title}}</h3>
-                <label>{{'Live poll'}} - {{new Date(pollingEvent.startDateTime).toLocaleDateString()}}</label>
-            </div>
-            <div v-if="!polls.length" class="w-full">
-                <InfoBox>
-                    {{$t('info.polls-will-appear')}}
-                </InfoBox>
-                <Spinner />
+        <div class="max-w-screen-md mx-auto px-4" :style="`height: 15vh`">
+            <div class="h-full flex flex-col justify-center">
+                <div class="text-white">
+                    <h3 class="font-bold">{{pollingEvent.title}}</h3>
+                    <label>{{'Live poll'}} - {{new Date(pollingEvent.startDateTime).toLocaleDateString()}}</label>
+                </div>
             </div>
         </div>
-        <div v-if="polls.length" class="w-full h-full">
-            <PollPopOver class="h-full w-full" :poll="polls[0]" :style="`min-height: calc(100vh - 350px);`"/>
+        <div v-if="!polls.length" class="w-full px-4 ">
+            <InfoBox>
+                {{$t('info.polls-will-appear')}}
+            </InfoBox>
+            <Spinner />
+        </div>
+        <div v-if="polls.length" class="w-full md:max-w-screen-md md:mx-auto h-full">
+            <PollPopOver class="h-full w-full" :poll="polls[0]" :style="`height: 75vh`"/>
         </div>
     </section>
 </template>
