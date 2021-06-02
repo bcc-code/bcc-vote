264e5bddeb35adcdebbf50f8b782e2735d4946f7
Philip <philip.dalen@bcc.no>
2021-06-02 22:08:02 +0200

    Added form validation and changed to socket connection to members

--- server/src/services/poll/poll.hooks.ts
+++ server/src/services/poll/poll.hooks.ts
@@ -1,16 +1,20 @@
-import * as authentication from '@feathersjs/authentication';
+import { HookContext } from "@feathersjs/feathers";
 // Don't remove this comment. It's needed to format import lines nicely.
 
-const { authenticate } = authentication.hooks;
+const validateAndFormat = (context: HookContext) => {
+  const { data } = context;
+  if (!data.title || data.title === '') throw Error('Validation Error: Please provide a title')
+  return context;
+};
 
 export default {
   before: {
     all: [],
     find: [],
     get: [],
-    create: [],
-    update: [],
-    patch: [],
+    create: [validateAndFormat],
+    update: [validateAndFormat],
+    patch: [validateAndFormat],
     remove: []
   },
 
