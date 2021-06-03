70e4819cbc143e38c02ed82f65e11d0632227854
JakubC-projects <jakub12134@gmail.com>
2021-06-03 09:39:59 +0200

    fixes

--- client/src/views/AdminPollResult.vue
+++ client/src/views/AdminPollResult.vue
@@ -8,20 +8,20 @@
 
 <script lang="ts">
 import {defineComponent} from 'vue'
-import { Answer } from '../domain';
+import { Poll, Answer } from '../domain';
 export default defineComponent({
   data(){
     return{
-      poll: {},
-      answers: [],
-      answerMap: {}
+      poll: {} as Poll,
+      answers: [] as Array<number>,
+      answerMap: {} as any
     }
   },
   async created(){
     this.poll = await this.$client.service('poll').get(this.$route.params.id)
     let ind = 0;
     this.poll.answers.forEach((ans: Answer) => {
-      this.answerMap[ans.answerId] = ind;
+      this.answerMap[ans.answerId.toString()] = ind;
       ind++;
     });
     this.answers = new Array<number>(ind).fill(0);
@@ -32,14 +32,14 @@ export default defineComponent({
       }
     })
     res.forEach((ans: Answer) => {
-      this.answers[this.answerMap[ans.answerId]] ++;
+      this.answers[this.answerMap[ans.answerId.toString()]] ++;
     });
     this.$client.service('answer').on('created', this.updateAnswers);
   },
   methods: {
     updateAnswers(ans: Answer){
       console.log('new ans');
-      this.answers[this.answerMap[ans.answerId]] ++;
+      this.answers[this.answerMap[ans.answerId.toString()]] ++;
     }
   }
 })
