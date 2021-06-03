0d537e5a9f4ca3cebf356c7e1ebcf6036c7f3b33
JakubC-projects <jakub12134@gmail.com>
2021-06-03 13:05:36 +0200

    log

--- server/src/channels.ts
+++ server/src/channels.ts
@@ -11,7 +11,6 @@ export default function(app: Application): void {
     return app.channel(data._key);
   });
   app.services.answer.publish('created', (data:any) => {
-    console.log(data.pollingEventId);
     return app.channel(data.pollingEventId);
   });
 
