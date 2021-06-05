0676e638a33251afa68dac6ab32cebe8bc78e2d1
JakubC-projects <jakub12134@gmail.com>
2021-06-05 12:47:39 +0200

    refresh information

--- client/src/components/log-information.vue
+++ client/src/components/log-information.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class="absolute log-container p-4 bg-black opacity-50 text-white">
-        <h4>Logging information</h4>
+    <div class="absolute log-container p-4 bg-black opacity-50 text-white" :key="refreshCount">
+        <h4>Logging information {{refreshCount}}</h4>
         connection: {{$client.io.connected}}
     </div>
 </template>
@@ -10,6 +10,21 @@
 import { defineComponent } from 'vue'
 
 export default defineComponent({
+    data(){
+        return {
+            refreshCount: 0
+        }
+    },
+    created(){
+        console.log(this.$client.io);
+        setTimeout( () => {
+            console.log('closing')
+            this.$client.io.close();
+        }, 2000)
+        setInterval( () => {
+            this.refreshCount ++;
+        }, 200)
+    }
 })
 </script>
 
