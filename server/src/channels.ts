e68266824504a90011b529d92a68047455d942ac
JakubC-projects <jakub12134@gmail.com>
2021-06-05 14:11:07 +0200

    channel mechanics change

--- server/src/channels.ts
+++ server/src/channels.ts
@@ -2,36 +2,48 @@ import '@feathersjs/transport-commons';
 import { Application } from './declarations';
 
 export default function(app: Application): void {
-  if(typeof app.channel !== 'function') {
-    return;
-  }
+    if(typeof app.channel !== 'function') {
+        return;
+    }
 
-  app.services['polling-event'].publish('patched', (data:any) => {
+    app.on('login', (payload:any, { connection }:any) => {
+        const lastChannel = connection.user.lastChannel;
+        if(lastChannel){
+            app.channel(lastChannel).join(connection);
+            console.log('joined', lastChannel);
+        }
+    });
 
-    return app.channel(data._key);
-  });
-  app.services.answer.publish('created', (data:any) => {
-    return app.channel(data.pollingEventId);
-  });
+    app.services['polling-event'].publish('patched', (data:any) => {
 
-  app.services.answer.publish('patched', (data:any) => {
-    return app.channel(data.pollingEventId);
-  });
+        return app.channel(data._key);
+    });
+    app.services.answer.publish('created', (data:any) => {
+        return app.channel(data.pollingEventId);
+    });
 
-  app.services.poll.publish('created', (data:any) => {
-    return app.channel(data.pollingEventId);
-  });
+    app.services.answer.publish('patched', (data:any) => {
+        return app.channel(data.pollingEventId);
+    });
 
-  app.services.poll.publish('patched', (data:any) => {
-    return app.channel(data.pollingEventId);
-  });
+    app.services.poll.publish('created', (data:any) => {
+        return app.channel(data.pollingEventId);
+    });
+
+    app.services.poll.publish('patched', (data:any) => {
+        return app.channel(data.pollingEventId);
+    });
 
 
-  app.services.poll.publish('updated', (data:any) => {
-    return app.channel(data.pollingEventId);
-  });
+    app.services.poll.publish('updated', (data:any) => {
+        return app.channel(data.pollingEventId);
+    });
 
-  app.services.poll.publish('removed', (data:any) => {
-    return app.channel(data.pollingEventId);
-  });
+    app.services.poll.publish('removed', (data:any) => {
+        return app.channel(data.pollingEventId);
+    });
+
+    app.on('disconnect', (connection) => {
+        console.log('disconnected', connection);
+    });
 }
