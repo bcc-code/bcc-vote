f937f3c6bf0797d68341a72613a1aafbc50a7fe1
Steven Malaihollo <steven.malaihollo@bcc.no>
2021-06-08 09:16:24 +0200

    Unshift and animate results feed

--- client/src/components/poll-popover.vue
+++ client/src/components/poll-popover.vue
@@ -71,14 +71,3 @@ export default defineComponent({
     }
 })
 </script>
-<style>
-.fade-enter-active,
-.fade-leave-active {
-  transition: opacity 0.3s ease;
-}
-
-.fade-enter-from,
-.fade-leave-to {
-  opacity: 0;
-}
-</style>
