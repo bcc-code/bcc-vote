f0b87e3e148f5a5678999486ad215c80df8beaf1
Steven Malaihollo <steven.malaihollo@bcc.no>
2021-06-08 16:12:35 +0200

    Topbar removal

--- client/src/components/layout-header.vue
+++ client/src/components/layout-header.vue
@@ -1,10 +1,10 @@
 <template>
-    <div  >
+    <div class="pb-12">
         <nav class="flex justify-between items-center fixed left-0 w-full h-12 z-10 shadow-md bg-white">
-            <div  v-if="!showBCCLogo" class="pl-5">
+            <div v-if="!showBCCLogo" class="pl-5">
                 <ArrowLeft class="cursor-pointer h-6 w-6 text-blue-800" @click="navigateBack"/>
             </div>
-            <div  v-if="showBCCLogo" class="pl-5">
+            <div v-if="showBCCLogo" class="pl-5">
                 
             </div>
             <div class="flex items-center py-2 pr-5">
@@ -12,7 +12,6 @@
                 <Logout/>  
             </div>          
         </nav>
-        <div class="h-12"></div>
     </div>
 </template>
 
@@ -22,7 +21,6 @@ import ArrowLeft from 'heroicons-vue3/outline/ArrowNarrowLeftIcon'
 import Logout from './logout.vue'
 
 import { defineComponent } from 'vue'
-import loadjs from "loadjs"
 
 export default defineComponent({
     components: {
@@ -38,37 +36,12 @@ export default defineComponent({
         },
     },
     methods: {
-        initTopbar() {
-            var scriptId = "script-bcc-topbar"
-            if (document.getElementById(scriptId) == null) {
-                var scriptPath = "https://widgets.bcc.no/widgets/TopbarJs"
-                loadjs([scriptPath,"https://widgets.bcc.no/styles/widgets.css"], {
-                    async: true,
-                    error: function (path) {
-                        console.error('could not load resource: ' + path)
-                    },
-                    before: function (path, element) {
-                        if(path === scriptPath ){
-                            element.id = scriptId
-                            element.setAttribute("data-authentication-type", "SPA")
-                            element.setAttribute(
-                                "data-authentication-location",
-                                "feathers-jwt"
-                            )
-                        }
-                    }
-                })
-            }
-        },
         navigateBack(){
             this.$router.go(-1)
         },
         logout(){
             this.$router.push({name:'logout'})
         }
-    },
-    mounted: function(){
-        this.initTopbar()
-    },
+    }
 })
 </script>
