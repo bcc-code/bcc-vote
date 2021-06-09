1c46eff0061400baabb7ba0cc9e207b264d2cecc
JakubC-projects <jakub12134@gmail.com>
2021-06-09 10:08:11 +0200

    some progress

--- server/test/channels-tests.ts
+++ server/test/channels-tests.ts
@@ -1,7 +1,7 @@
 import 'mocha';
 import { assert } from 'chai';
 import app from '../src/app';
-import { generateFreshContext }  from './setup-tests/test-set'
+import { generateFreshContext }  from './setup-tests/test-set';
 
 
 
@@ -9,32 +9,50 @@ import { generateFreshContext }  from './setup-tests/test-set'
 describe('channels', () => {
 
 
-  before(function() {
-    // server = app.listen(port);
-    // server.once('listening', () => done());
-  });
+    before(function() {
+        // server = app.listen(port);
+        // server.once('listening', () => done());
+    });
 
-  after(function() {
-    //server.close(done);
-  });
+    after(function() {
+        //server.close(done);
+    });
 
-  it('Add user to a polling-event channel', async () => {
-    try {
-      // Prepare
-      var context  = await generateFreshContext()
-      context.params.provider = ''
+    it('Add user to a polling-event channel via getting this event', async () => {
+        try {
+            // Prepare
+            const context  = await generateFreshContext();
+            context.params.provider = '';
 
-      // Act
-      let result = await app.service('polling-event').get('504279890',context.params)
-      let channels = app.channels
+            // Act
+            await app.service('polling-event').get('504279890',context.params);
+            const channels = app.channels;
 
-      // Assert
-      assert.equal(channels[0],'504279890')
-    } catch (error) {
-      assert.fail('There should be no error. Error:',error)
-    }
+            // Assert
+            assert.equal(channels[0],'504279890');
+        } catch (error) {
+            assert.fail('There should be no error. Error:',error);
+        }
 
-  });
+    });
+
+    it('Add user to a polling-event channel via getting a poll from this event', async () => {
+        try {
+            // Prepare
+            const context  = await generateFreshContext();
+            context.params.provider = '';
+
+            // Act
+            await app.service('poll').get('504310091',context.params);
+            const channels = app.channels;
+
+            // Assert
+            assert.equal(channels[0],'504279890');
+        } catch (error) {
+            assert.fail('There should be no error. Error:',error);
+        }
+
+    });
 
 
 
--- server/test/db-migrations-tests.ts
+++ server/test/db-migrations-tests.ts
@@ -6,44 +6,44 @@ import { Direction, MigrateWithConfig, deleteDatabase, pullDownTestDataLocally, 
 
 describe('Add-hock tests - db migrations', async () => {
 
-  it.skip('Apply migrations upwards', async () => {
+    it.skip('Apply migrations upwards', async () => {
 
-    try {
-      await deleteDatabase(getAranoDBConfigFromFeathers());
-      await MigrateWithConfig(Direction.Up, getAranoDBConfigFromFeathers(), './src/db-migrations');
-      assert.isOk('The migraiton works');
-    } catch (error) {
-      assert.fail('Migration did not import successfully');
-    }
-  });
+        try {
+            await deleteDatabase(getAranoDBConfigFromFeathers());
+            await MigrateWithConfig(Direction.Up, getAranoDBConfigFromFeathers(), './src/db-migrations');
+            assert.isOk('The migraiton works');
+        } catch (error) {
+            assert.fail('Migration did not import successfully');
+        } 
+    });
 
-  it.skip('Downgrade latest migration', async () => {
+    it.skip('Downgrade latest migration', async () => {
 
-    try {
-      await MigrateWithConfig(Direction.Down, getAranoDBConfigFromFeathers(), './src/db-migrations');
-      assert.isOk('The migraiton works');
-    } catch (error) {
-      assert.fail('Migration did not import successfully');
-    }
-  });
+        try {
+            await MigrateWithConfig(Direction.Down, getAranoDBConfigFromFeathers(), './src/db-migrations');
+            assert.isOk('The migraiton works');
+        } catch (error) {
+            assert.fail('Migration did not import successfully');
+        }
+    });
 
-  it.skip('Import test data', async () => {
+    it.skip('Import test data', async () => {
 
-    try {
-      const config:ArangoDBConfig = {
-        databaseName: "VOTE_TEST_TEMPLATE",
-        url: "http+ssl://fee1cf9d321a.arangodb.cloud:8529/",
-        auth: {
-          password: "root",
-          username: "import_test_data"
-        },
-        testDataPath:'.\\test\\setup-tests\\test_data'
-      };
+        try {
+            const config:ArangoDBConfig = {
+                databaseName: "VOTE_TEST_TEMPLATE",
+                url: "http+ssl://fee1cf9d321a.arangodb.cloud:8529/",
+                auth: {
+                    password: "root",
+                    username: "import_test_data"
+                },
+                testDataPath:'.\\test\\setup-tests\\test_data'
+            };
 
-      await pullDownTestDataLocally(config);
-      assert.isOk('Test data pulled down successfully');
-    } catch (error) {
-      assert.fail('There was a problem pulling down the test data');
-    }
-  });
+            await pullDownTestDataLocally(config);
+            assert.isOk('Test data pulled down successfully');
+        } catch (error) {
+            assert.fail('There was a problem pulling down the test data');
+        }
+    });
 });
