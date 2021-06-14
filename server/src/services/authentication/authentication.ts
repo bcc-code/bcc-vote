05f2736391d2f686a9ae70158a35a2600029c25d
Philip <philip.dalen@bcc.no>
2021-06-14 15:25:46 +0200

    Added fallback when members api is down

--- server/src/services/authentication/authentication.ts
+++ server/src/services/authentication/authentication.ts
@@ -21,36 +21,45 @@ class Auth0Strategy extends OAuthStrategy {
         const profile = await this.getProfile(authentication,params);
         const personID = profile["https://login.bcc.no/claims/personId"];
         try {
-            const userSvc = this.app?.services.user;
-            const memberSvc = this.app?.services.person;
-            let member = (await memberSvc.find({ query:{ personID: personID}})).data[0];
 
-            console.log(`Member with PersonID ${member.personID} succesfully retrieved from the members api`);
+            let member:any = {};
+            try {
+                member = (await this.app?.service('person').find({ query:{ personID: personID}})).data[0];
+                console.log(`AUTHENTICATE METHOD: Member with PersonID ${member.personID} succesfully retrieved from the members api`);
+                member = pick(member,['_id','_key','personID','churchID','related','email','cellPhone.formatted','church','displayName','age','roles', 'administrator']);
 
-            member = pick(member,['_id','_key','personID','churchID','related','email','cellPhone.formatted','church','displayName','age','roles', 'administrator']);
+                const { roles, activeRole} = getRolesForPerson(member);
 
-            const { roles, activeRole} = getRolesForPerson(member);
-            
-            member._id = `user/${member._key}`;
-            member.roles = roles;
-            member.activeRole = activeRole;
-            member.churchName = member.church.org.name;
-            delete member.church;
-            delete member.related;
+                member._id = `user/${member._key}`;
+                member.roles = roles;
+                member.activeRole = activeRole;
+                member.churchName = member.church.org.name;
+                delete member.church;
+                delete member.related;
 
-            const existingUsers = (await userSvc.find({ query: { _key: member._key }})).data;
+                const existingUsers = (await this.app?.service('user').find({ query: { _key: member._key }})).data;
 
-            if(existingUsers.length == 0) {
-                await userSvc.create(member);
-            } else if(existingUsers.length == 1) {
-                await userSvc.update(member._key,member);
+                if(existingUsers.length == 0) {
+                    await this.app?.service('user').create(member);
+                } else if(existingUsers.length == 1) {
+                    await this.app?.service('user').update(member._key,member);
+                }
+            } catch (error) {
+                console.error(error.message);
+                console.log(`AUTHENTICATE METHOD: Failed to retrieve member with personID:${personID} from the members api, please check if the members api is available and configured correctly`);
+                console.log(`AUTHENTICATE METHOD: Trying to retrieve member with personID:${personID} from local database`);
+                member = (await this.app?.service('user').find({ query:{ personID: personID}})).data[0];
+                if(member == undefined){
+                    throw new Error(`AUTHENTICATE METHOD: Failed to retrieve member with personID:${personID} from the members api and local database, its is therefore not possible to log the user in`);
+                }
+                console.log(`AUTHENTICATE METHOD: Member with PersonID ${personID} succesfully retrieved from the local user store. Please note that this is backup behaviour, the expected behaviour was that to retrieve the member from the members api.`);
             }
             return {
                 authentication: { strategy: this.name ? this.name : 'unknown' },
                 [entity]: member
             };
         } catch(err) {
-            throw new Error('Login not possible');
+            throw err;
         }
     }
 }
