6a3f8d11204101715517adeac4f6395ce0f47b46
Steven Malaihollo <steven.malaihollo@bcc.no>
2021-06-30 17:24:15 +0200

    User role for AdminRole orgs

--- server/src/services/authentication/authentication-helpers.ts
+++ server/src/services/authentication/authentication-helpers.ts
@@ -1,18 +1,17 @@
-import { Role, RoleName } from '../../domain';
+import { UserRole, RoleName } from '../../domain';
 
 const rolesInUseInApp = ['CentralAdministrator','SentralInformasjonsmedarbeider','Developer','VotingAdmin','Member'];
 
-
-export function getActiveRole(userRoles:any):RoleName{
+export function getActiveRole(userRoles:Array<UserRole>):RoleName{
     let activeRole = 'None' as RoleName;
-    const possibleActiveRoles = userRoles.filter((role:Role) => rolesInUseInApp.includes(role.enumName));
+    const possibleActiveRoles = userRoles.filter((role:UserRole) => rolesInUseInApp.includes(role.enumName));
 
     // Sort the permission from highest level to lowest level
-    possibleActiveRoles.sort(function (a:Role, b:Role) {
+    possibleActiveRoles.sort(function (a:UserRole, b:UserRole) {
         return a.securityLevel - b.securityLevel;
     });
 
-    if(possibleActiveRoles.some((role:Role) => role.enumName === 'VotingAdmin')) { 
+    if(possibleActiveRoles.some((role:UserRole) => role.enumName === 'VotingAdmin')) { 
         activeRole = 'VotingAdmin';
     } else {
         activeRole = possibleActiveRoles[0].enumName;
