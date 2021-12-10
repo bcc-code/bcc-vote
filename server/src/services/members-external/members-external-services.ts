ef49c51d4f286cea503d39efb67591d2849abbed
mikolaj.kubista <mikolaj.kubista@gmail.com>
2021-12-10 15:37:03 +0100

    membesAPI connection as REST

--- server/src/services/members-external/members-external-services.ts
+++ server/src/services/members-external/members-external-services.ts
@@ -1,42 +1,26 @@
 import logger from '../../logger';
 import { Application } from '../../declarations';
-import socketio from '@feathersjs/socketio-client';
-import { inspect } from 'util';
-const io = require('socket.io-client');
-
 import feathers from '@feathersjs/feathers';
-
+import rest from '@feathersjs/rest-client';
+import axios from 'axios';
 
 export default function (app: Application): void {
 
     const membersConfig = app.get("members");
-    
+
     const url = membersConfig.url;
 
-    const socket = io(url, {
-        transports:["websocket", "polling"],
-        extraHeaders: {
-            'x-access-token': membersConfig.apiKey
-        }
-    });
-
-
-    socket.on("connect", () => {
-        logger.info(`[SOCKET_EVENT] [VOTING_APP] [CONNECTED] socket successfully connected to ${url}`);
-    });
-
-    socket.on("disconnect", (reason: string) => {
-        logger.info(`[SOCKET_EVENT] [VOTING_APP] [DISCONNECTED] socket was disconnected from ${url} for the following reason: ${reason}`);
-    });
-
-    socket.on("connect_error", (error: any) => {
-        logger.error(`[SOCKET_EVENT] [VOTING_APP] [ERROR] socket failed to reconnect to ${url} after an reconnection attempt with the following error: ${inspect(error)}`);
-    });
-
     const membersClient = feathers();
+    const restClient = rest(url);
 
-    membersClient.configure(socketio(socket, {
-        timeout: 4000
+    membersClient.configure(restClient.axios(axios, {
+        timeout: 5000,
+        headers: {
+            'x-api-key': membersConfig.apiKey,
+        },
+        errorHandler: function (error: any) {
+            logger.error(error);
+        }
     }));
 
     app.use('/person', membersClient.service('person'));
