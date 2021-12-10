05c15bf6991923f2cd1528af0b2251c10a31acae
mikolaj.kubista <mikolaj.kubista@gmail.com>
2021-12-10 17:40:40 +0100

    two clients

--- server/src/services/members-external/members-external-services.ts
+++ server/src/services/members-external/members-external-services.ts
@@ -1,8 +1,14 @@
 import logger from '../../logger';
 import { Application } from '../../declarations';
+import socketio from '@feathersjs/socketio-client';
+import { inspect } from 'util';
 import feathers from '@feathersjs/feathers';
 import rest from '@feathersjs/rest-client';
 import axios from 'axios';
+const io = require('socket.io-client');
+
+
+
 
 export default function (app: Application): void {
 
@@ -10,10 +16,38 @@ export default function (app: Application): void {
 
     const url = membersConfig.url;
 
-    const membersClient = feathers();
-    const restClient = rest(url);
+    const socket = io(url, {
+        transports:["websocket", "polling"],
+        extraHeaders: {
+            'x-access-token': membersConfig.apiKey
+        }
+    });
 
-    membersClient.configure(restClient.axios(axios, {
+
+    socket.on("connect", () => {
+        logger.info(`[SOCKET_EVENT] [VOTING_APP] [CONNECTED] socket successfully connected to ${url}`);
+    });
+
+    socket.on("disconnect", (reason: string) => {
+        logger.info(`[SOCKET_EVENT] [VOTING_APP] [DISCONNECTED] socket was disconnected from ${url} for the following reason: ${reason}`);
+    });
+
+    socket.on("connect_error", (error: any) => {
+        logger.error(`[SOCKET_EVENT] [VOTING_APP] [ERROR] socket failed to reconnect to ${url} after an reconnection attempt with the following error: ${inspect(error)}`);
+    });
+
+    const membersWebSocketClient = feathers();
+
+    membersWebSocketClient.configure(socketio(socket, {
+        timeout: 4000
+    }));
+
+    app.use('/org', membersWebSocketClient.service('org'));
+    app.use('/role', membersWebSocketClient.service('role'));
+
+    const restClient = rest(url);
+    const membersRestClient = feathers();
+    membersRestClient.configure(restClient.axios(axios, {
         timeout: 5000,
         headers: {
             'x-access-token': membersConfig.apiKey,
@@ -26,7 +60,6 @@ export default function (app: Application): void {
         }
     }));
 
-    app.use('/person', membersClient.service('person'));
-    app.use('/org', membersClient.service('org'));
-    app.use('/role', membersClient.service('role'));
+    app.use('/person', membersRestClient.service('person'));
+
 }
