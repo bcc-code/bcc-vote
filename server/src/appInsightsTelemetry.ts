4a8b8bb31bb2b4850d35ca45fe4e174359bb4fbf
mikolaj.kubista <mikolaj.kubista@gmail.com>
2022-01-27 13:28:55 +0100

    changed role name

--- server/src/appInsightsTelemetry.ts
+++ server/src/appInsightsTelemetry.ts
@@ -5,7 +5,7 @@ if (process.env.BCC_VOTE_APPINSIGHTS_CONNECTIONSTRING) {
         .setSendLiveMetrics(true)
         .setAutoDependencyCorrelation(true)
         .start();
-    appInsights.defaultClient.context.tags[appInsights.defaultClient.context.keys.cloudRole] = 'members-backend';
+    appInsights.defaultClient.context.tags[appInsights.defaultClient.context.keys.cloudRole] = 'vote-backend';
 }
 
 export default appInsights;
