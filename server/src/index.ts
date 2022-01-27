674fe3d4b368b5f2ba1fd985d48a355e6ac2ce26
mikolaj.kubista <mikolaj.kubista@gmail.com>
2022-01-27 13:29:54 +0100

    load appInsights and track startup time

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,14 +1,18 @@
+import appInsights from './appInsightsTelemetry';
 import app from './app';
 import logger from './logger';
 
 const port = app.get('port');
 const server = app.listen(port);
+const startTime = Date.now();
 
 process.on('unhandledRejection', (reason, p) => {
     const message = ['UnhandeledRejection', p, reason].join(' ');
     logger.error(message);
 });
 
-server.on('listening', () =>
-    logger.info('Feathers application started on http://%s:%d', app.get('host'), port)
-);
+server.on('listening', () => {
+    logger.info('Feathers application started on http://%s:%d', app.get('host'), port);
+    const duration = Date.now() - startTime;
+    appInsights?.defaultClient?.trackMetric({name: 'Server startup time', value: duration});
+});
