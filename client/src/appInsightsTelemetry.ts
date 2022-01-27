82e621973e66dc346ea803f10100b50a0e280974
mikolaj.kubista <mikolaj.kubista@gmail.com>
2022-01-27 16:42:00 +0100

    add insight init

---
+++ client/src/appInsightsTelemetry.ts
@@ -0,0 +1,17 @@
+import determineConfigBasedOnEnvironment from './config';
+import {ApplicationInsights} from '@microsoft/applicationinsights-web';
+
+const appInsights = new ApplicationInsights({
+    config: {
+        connectionString: determineConfigBasedOnEnvironment().appInsisghtKey,
+        enableAutoRouteTracking: true,
+        disableFetchTracking: false,
+    },
+});
+appInsights.loadAppInsights();
+appInsights.trackPageView();
+appInsights.addTelemetryInitializer((envelope: any) => {
+    envelope.tags['ai.cloud.role'] = 'vote-frontend';
+});
+
+export default appInsights;
