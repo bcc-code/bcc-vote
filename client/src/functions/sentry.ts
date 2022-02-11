0336ca25448f1f187089323995d6a7a923d8dc2b
mikolaj.kubista <mikolaj.kubista@gmail.com>
2022-02-11 17:09:24 +0100

    remove other origins

--- client/src/functions/sentry.ts
+++ client/src/functions/sentry.ts
@@ -11,7 +11,7 @@ export const initSentry = (app: App<Element> , router: Router):void =>{
         integrations: [
             new BrowserTracing({
                 routingInstrumentation: vueRouterInstrumentation(router),
-                tracingOrigins: ["localhost", "vote.bcc.no", "dev.vote.bcc.no", "beta.vote.bcc.no",/^\//],  
+                tracingOrigins: ["vote.bcc.no",/^\//],  
             }),
         ],
         tracesSampleRate: 1.0,
