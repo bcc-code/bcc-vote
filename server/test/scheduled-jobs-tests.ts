75c68a6e72996eb26eb0b2c747fa081c89ed4c67
Steven Malaihollo <stevenjanm@gmail.com>
2022-06-22 16:10:28 +0200

    FIx scheduled jobs test

--- server/test/scheduled-jobs-tests.ts
+++ server/test/scheduled-jobs-tests.ts
@@ -1,7 +1,7 @@
 import 'mocha';
 import { assert } from 'chai';
 import app from '../src/app';
-import { getAranoDBConfigFromFeathers, pollingEventsTestSet }  from './setup-tests/test-set';
+import { getAranoDBConfigFromFeathers, pollingEventsTestSet } from './setup-tests/test-set';
 import { PollingEvent, PollingEventStatus } from '../src/domain';
 import { importDB} from "@bcc-code/arango-migrate";
 import sinon from 'sinon';
@@ -21,20 +21,7 @@ describe('Scheduled jobs', async () => {
         sinon.restore();
     });
 
-    it('Starting server with no active events does not schedule answer batching', async () => {
-        try {            
-            const activePollingEvents = await app.services['polling-event'].find({ query: { status: PollingEventStatus['Live']}});
-            assert.equal(activePollingEvents.length, 0);
-            
-            const stubbedAnswerBatchingSvc = sinon.stub(app.services['answer-batch'], 'create');
-            await sleep(2000);
-            assert.equal(stubbedAnswerBatchingSvc.notCalled, true);
-        } catch (error) {
-            assert.fail(error.message);
-        }
-    });
-
-    it('Starting server with active event schedules answer batching', async () => {
+    it('Activating an event schedules answer batching', async () => {
         try {
             await app.services['polling-event'].patch(pollingEvent._key, { status: PollingEventStatus['Live']},{});
 
@@ -44,6 +31,22 @@ describe('Scheduled jobs', async () => {
             assert.equal(stubbedAnswerBatchingSvc.calledOnce, true);
             await sleep(1000);
             assert.equal(stubbedAnswerBatchingSvc.calledTwice, true);
+        } catch (error) {
+            assert.fail(error.message);
+        }
+    });
+
+    it('Deactivating an event disables answer batching', async () => {
+        try {
+            await app.services['polling-event'].patch(pollingEvent._key, { status: PollingEventStatus['Finished']},{});
+
+            const stubbedAnswerBatchingSvc = sinon.stub(app.services['answer-batch'], 'create');
+            await sleep(1500);
+
+            const activePollingEvents = await app.services['polling-event'].find({ query: { status: PollingEventStatus['Live']}});
+            assert.equal(activePollingEvents.length, 0);
+            
+            assert.equal(stubbedAnswerBatchingSvc.notCalled, true);
         } catch (error) {
             assert.fail(error.message);
         }
