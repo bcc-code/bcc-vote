f5ba7cfba00e09a72f7473c28f377253fefc3514
Jakub Czyz <jakub12134@gmail.com>
2022-06-22 09:22:56 +0200

    Chagne answer

--- server/test/load-test.ts
+++ server/test/load-test.ts
@@ -1,14 +1,11 @@
 import 'mocha';
-import { assert } from 'chai';
-import { Server } from 'http';
-const socketio = require('@feathersjs/socketio-client');
-const io = require('socket.io-client');
+import socketio from '@feathersjs/socketio-client';
+import io from 'socket.io-client';
 import feathers from '@feathersjs/feathers';
-import { getFeahtersToken }  from './setup-tests/test-set'
 import app from '../src/app';
-
-
-
+import fs from 'fs';
+import jwt from 'jwt-simple';
+import logger from '../src/logger';
 
 // -----------------------------------------------------------------------------------------
 // NB!!! Before running this performance test you have to run
@@ -19,87 +16,113 @@ import app from '../src/app';
 // -----------------------------------------------------------------------------------------
 
 describe('load test', () => {
-    const testingVariables = app.get('testingSet')
-    const host = app.get('host')
-    const protocol = app.get('protocol')
-
-  it.skip('Perform a socket load test on an environment', async (done) => {
-
-    try {
-      let numberOfConnections = 1500
-      let counter = 1
-      let clientsPromises = []
-      while (counter != numberOfConnections) {
-        clientsPromises.push(newFeathersClient())
-        console.log('new conainer added no:',counter)
-        counter++
-      }
-
-      var clients = await Promise.all(clientsPromises)
-     //var answer = await clients[0].service('answer').get('1340021880',{})
-      var a = {
-        "_id": testingVariables.answerId,
-        "_from": testingVariables.pollId,
-        "_to": testingVariables.userId,
-        "answerId": 1623243249532,
-        "pollingEventId": "1339667582",
-        "displayName": "Philip Dalen",
-        "churchName": "Oslo/Follo",
-        "lastChanged": 1623335253622
-      }
+    const testingVariables = app.get('testingSet');
+    const host = app.get('host');
+    const protocol = app.get('protocol');
+    let allAnswers = 0;
+    let connectedClients = 0;        
+    const numberOfConnections = 600;
+    it.only('Perform a socket load test on an environment', async () => {
 
 
-      var answer = await clients[0]?.service('answer').create(a,{})
-      var deletedAnswer = await clients[0]?.service('answer').remove(answer._key,{})
+        const clientsPromises:Promise<feathers.Application<any>>[] = [];
+        for (let i = 1; i <= numberOfConnections; i++) {
+            clientsPromises.push(newFeathersClient(i));
+            // console.log('new user added, personId:',personId);
+        }
 
-      let events = await  clients[0]?.service('polling-event').find({}) as any[];
+        const clients = await Promise.all(clientsPromises);
+        for (let i = 1; i <= numberOfConnections; i++)  {
+            runFlow(clients[i - 1], i);
+        }
 
-      while (true) {
-        await sleep(100)
-      }
-
-    } catch (error) {
-      assert.fail(error.message)
-    }
-  });
-
-
-async function newFeathersClient() {
-  try {
-
-    let url = `${protocol}://${host}`
-      let token = await getFeahtersToken()
-      const socket = io(url, {
-        transports:["websocket", "polling"],
-        extraHeaders: {
-          'Authorization': `bearer ${token}`
+        while (true) {
+            await sleep(5000);
+            console.log('allAnswers:',allAnswers,"/",numberOfConnections*numberOfConnections);
+            if(allAnswers === numberOfConnections*numberOfConnections) break;
         }
     });
 
-      const membersClient = feathers()
-
-      membersClient.configure(socketio(socket, {
-        timeout: 400000
-      }));
+    async function runFlow(client: feathers.Application<any>, personId: number) {
 
 
-      var event = await membersClient.service('polling-event').get(testingVariables.pollingEentId,{})
-      console.log('Socket was added to the polling-event channel')
+        const a = {
+            _from: testingVariables.pollId,
+            _to: `person/${personId}`,
+            answerId: testingVariables.answerId,
+            pollingEventId: testingVariables.pollingEventId,
+            visibility: "public",
+        };
+        try {
+            await client.service('answer').create(a,{});
 
-      membersClient.service('answer').on('created',(a:any)=>{
-        console.log('[RECEIVED PUBLISHED ANSWER IN CLIENT]')
-      })
+        } catch(err) {
+            logger.error(err.message);
+            throw err;
+        }
+    }
 
-      return membersClient
 
-  } catch (error) {
-    console.log(error)
-  }
+    async function newFeathersClient(personId: number) {
 
-}
+        const url = `${protocol}://${host}`;
+        const token = await getNewAuth0Jwt(personId);
+        const socket = io(url, {
+            transports:["websocket", "polling"],
+            extraHeaders: {
+                'Authorization': `bearer ${token}`
+            }
+        } as any);
+        const membersClient = feathers();
 
-async function sleep(msec:any) {
-  return new Promise(resolve => setTimeout(resolve, msec));
-}
+        membersClient.configure(socketio(socket, {
+            timeout: 400000
+        }));
+        await membersClient.service('polling-event').get(testingVariables.pollingEventId,{});
+        membersClient.service('answer').on('created',(a:any)=>{
+            // console.log(`[CLIENT ${personId}] Received answer ${a._id}`);
+            allAnswers++;
+        });
+
+        membersClient.on("disconnect", () => {
+            logger.error(`[CLIENT ${personId}] Disconnected`);
+        });
+        connectedClients ++;
+        console.log("Connected clients:", connectedClients, "/", numberOfConnections);
+        return membersClient;
+    }
+
+    async function sleep(msec:any) {
+        return new Promise(resolve => setTimeout(resolve, msec));
+    }
 
 });
+
+
+function getNewAuth0Jwt(personID = 54512) {
+    const privateKey = fs.readFileSync('config/development-private.key').toString();
+
+    const now = Date.now();
+
+    const expires = now + 7 * 24 * 60 * 60 * 1000;
+    const access_token = jwt.encode(
+        {
+            'https://login.bcc.no/claims/physicalLoginTimestamp': Date.now(),
+            'https://login.bcc.no/claims/personId': personID,
+            'https://members.bcc.no/app_metadata': {
+                hasMembership: true,
+                personId: personID,
+            },
+            iss: 'auth0-impersonation',
+            sub: 'auth0|5e90e4a9acb4320bd72e9bde',
+            aud: ['bcc.members'],
+            iat: now,
+            exp: expires,
+            azp: 'mHD7Uto7xPmyo4nVA2okg6CJCxjCDQe3',
+            scope: 'openid profile email members.mfa',
+        },
+        privateKey,
+        'RS256'
+    );
+    return access_token;
+}
