d34b4fda25a096c071ca298125895b48cc72b9cd
Steven Malaihollo <stevenjanm@gmail.com>
2022-07-20 17:17:04 +0200

    Load test for firebase connections

--- server/test/load-test.ts
+++ server/test/load-test.ts
@@ -2,12 +2,13 @@ import 'mocha';
 import socketio from '@feathersjs/socketio-client';
 import io from 'socket.io-client';
 import feathers from '@feathersjs/feathers';
-import app from '../src/app';
 import fs from 'fs';
 import jwt from 'jwt-simple';
 import logger from '../src/logger';
 import { AuthenticationRequest } from '@feathersjs/authentication';
-
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
 // -----------------------------------------------------------------------------------------
 // NB!!! Before running this performance test you have to run
 // npm run mocha-dev-in-cloud
@@ -22,28 +23,50 @@ interface VirtualUser {
     token: string
 }
 
-describe('load test', () => {
-    const testingVariables = app.get('testingSet');
+const testingVariablesLocal = {
+    pollingEventId:"84441",
+    pollId:"poll/84588",
+    answerId: "1655972543954",
+    host: "localhost:4040",
+    protocol: "http"
+};
+const testingVariablesDev = {
+    pollingEventId:"1520133464",
+    pollId:"poll/1524149878",
+    answerId: "1657097392329",
+    host: "dev.vote.bcc.no",
+    protocol: "https"
+};
+
+describe.skip('load test', () => {
+    const useLocal = true;
+    const testingVariables = useLocal ? testingVariablesLocal : testingVariablesDev;
 
     let receivedAnswersTotal = 0;
     let connectedClients = 0;
-    const numberOfConnections = 600;
-    it.only('Perform a socket load test on an environment', async (done) => {
+    const numberOfConnections = 300;
+    const startupDate = Date.now();
+    const removeListeners: (() => void)[] = [];
 
-        const connetionPromises:Promise<void>[] = [];
+    afterEach(() => {
+        removeListeners.forEach((unsubscribe) => unsubscribe());
+    });
+
+    it('Perform a socket load test on an environment', (done) => {
+        const connectionPromises:Promise<void>[] = [];
         const virtualUsers:VirtualUser[] = [];
         for (let i = 1; i <= numberOfConnections; i++) {
             const vu = createNewVirtualUser(i);
             virtualUsers.push(vu);
-            connetionPromises.push(setupUser(vu));
+            connectionPromises.push(setupUser(vu));
         }
 
-        await Promise.all(connetionPromises);
-
-        for(const vu of virtualUsers){
-            runFlow(vu);
-        }
-        setInterval(checkStatus, 5000, done);
+        Promise.all(connectionPromises).then(() => {
+            for(const vu of virtualUsers){
+                runFlow(vu);
+            }
+            setInterval(checkStatus, 5000, done);
+        });
     });
 
     async function runFlow(vu: VirtualUser) {
@@ -64,36 +87,62 @@ describe('load test', () => {
     }
 
     async function setupUser(vu: VirtualUser) {
-
-        const authRequest: AuthenticationRequest= {
-            strategy: 'jwt',
-            accessToken: vu.token
-        };
-        const authResult = await vu.client.service('authentication').create(authRequest, {});
-        if(authResult.firebaseAccessToken) {
-            console.log('Token!');
+        try {
+            const authRequest: AuthenticationRequest= {
+                strategy: 'jwt',
+                accessToken: vu.token
+            };
+            const {firebaseAccessToken} = await vu.client.service('authentication').create(authRequest, {});
+    
+            if(firebaseAccessToken) {
+                const app = await initFirestore(firebaseAccessToken, vu.personId);
+            
+                const answerCol = app.firestore().collection('answer');
+                const answerRef = answerCol.where('lastChanged', '>=', startupDate);
+                const unsubscribeAnswer = answerRef.onSnapshot(snap => {
+                    snap.docChanges().forEach(change => {
+                        if (change.type === 'added') {
+                            receivedAnswersTotal++;
+                        }
+                    });
+                });
+                removeListeners.push(unsubscribeAnswer);
+            }
+    
+            vu.client.on("disconnect", () => {
+                logger.error(`[CLIENT ${vu.personId}] Disconnected`);
+            });
+            connectedClients ++;
+            console.log("Connected clients:", connectedClients, "/", numberOfConnections);
+        } catch(err) {
+            logger.error(`[CLIENT ${vu.personId}] Failed to setup: ${err.message}`);
         }
-        vu.client.service('answer').on('created',()=>{
-            receivedAnswersTotal++;
-        });
+    }
 
-        vu.client.on("disconnect", () => {
-            logger.error(`[CLIENT ${vu.personId}] Disconnected`);
-        });
-        connectedClients ++;
-        console.log("Connected clients:", connectedClients, "/", numberOfConnections);
+    async function initFirestore(token: string, personID:number) {
+        const app = firebase.initializeApp({
+            "apiKey": "AIzaSyDt48CpGQBSsZg-6SSLTmLHgSyDgLVLmzE",
+            "authDomain": "bcc-vote.firebaseapp.com",
+            "projectId": "bcc-vote",
+            "storageBucket": "bcc-vote.appspot.com",
+            "messagingSenderId": "720418204616",
+            "appId": "1:720418204616:web:19533b48520a110f252efd",
+            "measurementId": "G-4KNVYNZ55W"
+        }, `firebase-for-${personID}`);
+        const auth = app.auth();
+        await auth.signInWithCustomToken(token);
+        return app;
     }
 
     function createNewVirtualUser(personId: number):VirtualUser {
-        const host = app.get('host');
-        const protocol = app.get('protocol');
+        const {host, protocol} = testingVariables;
 
         const url = `${protocol}://${host}`;
         const token = getNewAuth0Jwt(personId);
         const socket = io(url, {
             transports:["websocket", "polling"],
             extraHeaders: {
-                'Authorization': `bearer ${token}`
+                'Authorization': `Bearer ${token}`
             }
         } as any);
         const client = feathers();
